60e09cfe20f316b3f03c84859ef205978146dbbb
Luis Hiram Diaz Sanchez <luishiram.ds@gmail.com>
2026-07-04 22:46:37 -0600

    feat: integrate framer-motion for animations and add product display in chat messages

--- frontend/ramon-chat/src/components/chat/ChatMessages.tsx
+++ frontend/ramon-chat/src/components/chat/ChatMessages.tsx
@@ -1,5 +1,6 @@
 import type { ChatMessage } from '@/types/chat';
 import MessageBubble from './MessageBubble';
+import { WelcomeScreen } from '../helpers/WelcomeScreen';
 
 const messages: ChatMessage[] = [
    {
@@ -18,15 +19,44 @@ const messages: ChatMessage[] = [
       content:
          'Un switch PoE permite transmitir datos y alimentación eléctrica por el mismo cable Ethernet.',
    },
+   {
+      id: '4',
+      role: 'assistant',
+      content: 'Aqui tienes 3 productos que relacionados a tu request',
+      products: [
+         {
+            id: 1,
+            description: 'Camera 1',
+            name: 'Camara Sony',
+            price: 15000,
+         },
+         {
+            id: 2,
+            description: 'Camera 1',
+            name: 'Camara Sony',
+            price: 15000,
+         },
+         {
+            id: 3,
+            description: 'Camera 1',
+            name: 'Camara Sony',
+            price: 15000,
+         },
+      ],
+   },
 ];
 
 export default function ChatMessages() {
    return (
       <div className='flex-1 overflow-y-auto p-6'>
          <div className='mx-auto flex w-full max-w-4xl flex-col gap-6'>
-            {messages.map((message) => (
-               <MessageBubble key={message.id} message={message} />
-            ))}
+            {messages.length === 0 ? (
+               <WelcomeScreen />
+            ) : (
+               messages.map((message) => (
+                  <MessageBubble key={message.id} message={message} />
+               ))
+            )}
          </div>
       </div>
    );
--- frontend/ramon-chat/src/components/chat/MessageBubble.tsx
+++ frontend/ramon-chat/src/components/chat/MessageBubble.tsx
@@ -1,4 +1,7 @@
 import type { ChatMessage } from '@/types/chat';
+import { motion } from 'framer-motion';
+import { Avatar } from '../helpers/Avatar';
+import { ProductCard } from '../helpers/ProductCard';
 
 interface Props {
    message: ChatMessage;
@@ -8,21 +11,30 @@ export default function MessageBubble({ message }: Props) {
    const isUser = message.role === 'user';
 
    return (
-      <div className={`flex ${isUser ? 'justify-end' : 'justify-start'}`}>
+      <motion.div
+         initial={{ opacity: 0, y: 6 }}
+         animate={{ opacity: 1, y: 0 }}
+         className={`flex gap-3 my-4 ${isUser ? 'justify-end' : 'justify-start'}`}
+      >
+         {!isUser && <Avatar role='assistant' />}
+
          <div
-            className={`
-          max-w-[75%]
-          rounded-2xl
-          px-5
-          py-3
-          text-sm
-          leading-7
-          shadow-sm
-          ${isUser ? 'bg-blue-600 text-white' : 'bg-slate-100 text-slate-900'}
-        `}
+            className={`max-w-[75%]  px-4 py-3 rounded-2xl text-sm leading-relaxed ${
+               isUser
+                  ? 'bg-blue-600 text-white rounded-br-sm'
+                  : 'bg-gray-100 text-gray-900 rounded-bl-sm'
+            }`}
          >
-            {message.content}
+            {message.content && <p>{message.content}</p>}
+            <div className='flex '>
+               {message.products &&
+                  message.products.map((p) => (
+                     <ProductCard key={p.id} product={p} />
+                  ))}
+            </div>
          </div>
-      </div>
+
+         {isUser && <Avatar role='user' />}
+      </motion.div>
    );
 }
